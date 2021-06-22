58c80f0521d62d769ef6699e123878a6aca04001
mitej23 <mitejmadan@gmail.com>
2021-06-22 23:59:21 +0530

    added socket io

--- server/server.js
+++ server/server.js
@@ -3,14 +3,22 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:19006", "http://192.168.2.8:19006","http://192.168.43.43:19000/"],
+  },
+});
+
+app.get("/api", (req, res) => {
+  res.send("hellow world");
+});
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log(socket.id);
 });
 
 server.listen(3000, () => {
