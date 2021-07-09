7c51d67d2ecff5952ffe31e8d9262a2d3e7fcd7b
mitej23 <mitejmadan@gmail.com>
2021-07-09 17:56:06 +0530

    added client code for user

--- client-management/App.js
+++ client-management/App.js
@@ -1,21 +1,95 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState, useRef } from "react";
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+
+import io from "socket.io-client";
+//Don't usee localhost while testing on phone
+var socket = io("http://localhost:3000/", {
+  transports: ["websocket"],
+  jsonp: false,
+});
 
 export default function App() {
+  let [name, setName] = useState("");
+  let [userType, setUserType] = useState("");
+  let [id, setId] = useState("");
+  let [isSubmitted, setSubmitted] = useState(false);
+  let [orderId, setOrderId] = useState(1234);
+  
+
+  useEffect(() => {
+    setOrderId(Math.floor(1000 + Math.random() * 9000));
+
+    socket.on("connect", () => {
+      setId(socket.id);
+    });
+
+    socket.on("order", (data) => {
+      console.log("order placed ", data.name, data.orderId);
+      orderhandler(ordersRef.current, setOrders, data);
+    });
+
+    return () => {
+      socket.removeAllListeners();
+    };
+  }, []);
+
+
+  const orderhandler = (orders, setOrders, { name, orderId, userId }) => {
+    console.log(orders);
+    setOrders((orders) => [...orders, { name, orderId, taken: false, userId }]);
+  };
+
+  
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (name == "") {
+      alert("fields are empty!! Fill them");
+      return;
+    }
+    setUserType("user");
+    setSubmitted(true);
+  };
+
+  const placeOrder = () => {
+    //broadcast the message in group room
+    console.log("order placed", name, orderId);
+    socket.emit("order-placed", name, orderId, id);
+    setOrderId(Math.floor(1000 + Math.random() * 9000));
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>TEst</Text>
-      <StatusBar style="auto" />
+      {isSubmitted ? (
+        <>
+          <Text>Name: {name}</Text>
+          <Text>User Type: {userType}</Text>
+          <Text>Socket Id:{id}</Text>
+
+          <>
+            <Text>Order ID: {orderId}</Text>
+            <Button title="Press me" onPress={placeOrder} />
+          </>
+        </>
+      ) : (
+        <>
+          <Text>Form:</Text>
+          <Text>Name:</Text>
+          <TextInput onChangeText={setName} value={name} />
+
+          <Button style={{ marginTop: 20 }} title="Press e" onPress={submit} />
+        </>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    width: 300,
+    marginTop: 55,
+    marginLeft: 10,
   },
 });
